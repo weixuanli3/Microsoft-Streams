cf3a6a2bc58ba03b0419b69d603dc5a41477ee8f
Tomas Ostroumoff <z5312982@ad.unsw.edu.au>
2021-09-22 22:08:04 +1000

    very rough first draft for the auth_login function

--- src/auth.py
+++ src/auth.py
@@ -2,6 +2,22 @@ from src.data_store import data_store
 from src.error import InputError
 
 def auth_login_v1(email, password):
+    # Do some for loop that loops through the users list somehow and compares the given email to
+    found = 0
+    i = 0
+    while (found == 0):
+        # reference some the user list
+        if (users[i]['email'] == email):
+            found == 1
+            if (users[i]['password'] == password):
+                return user[i]['u_id']
+            else:
+                ##Spit out an input error for the wrong password
+            return 
+        
+        i+= 1
+    
+    ## return an input error for the email not being found
     return {
         'auth_user_id': 1,
     }
