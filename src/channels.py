14cd2663018a31d10d7d317a77f90b1b14a0e4da
Joseph <joseph.shao2003@gmail.com>
2021-09-26 14:43:13 +1000

    Added code to be in sync with new data

--- src/channels.py
+++ src/channels.py
@@ -27,24 +27,27 @@ def channels_create_v1(auth_user_id, name, is_public):
     for user in user_data:
         if auth_user_id == {user['id']}:
             user_exists = True
-            if len(name) < 1 or len(name) > 20:
-                raise InputError("Invalid channel name")
-            user['channels'].add(name)
+            curr_user = user
     
     if not user_exists:
         raise InputError("User doesn't exist")
+    
+    if len(name) < 1 or len(name) > 20:
+        raise InputError("Invalid channel name")
     
     channel_data = data_store.get_data()['channels']
     for channel in channel_data:
         if name == channel['name']:
             raise InputError("Channel name already exists")
     new_channel_id = len(channel_data) + 1
+    
+    curr_user['channels'].add(new_channel_id)
     channel_data.append({
         'chan_id': new_channel_id,
         'name': name,
-        'owner': name,
-        'users': {auth_user_id},
-        'public': is_public
+        'owner_id': auth_user_id,
+        'users_id': {auth_user_id},
+        'is_public': is_public
     })
     return {
         'channel_id': new_channel_id,
