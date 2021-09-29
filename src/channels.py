170325872f99002c2ab8b84052c06bec62c4b4f0
Joseph <joseph.shao2003@gmail.com>
2021-09-29 11:14:35 +1000

    Pylint is now 10/10, added doc-strings for the functions

--- src/channels.py
+++ src/channels.py
@@ -1,7 +1,36 @@
+''' Contains the functions to create a new channel and to list pub/priv channels'''
 from src.data_store import data_store
 from src.error import InputError
 
 def channels_list_v1(auth_user_id):
+    '''
+    Function will take in an user_id and list all the public channels the user is in
+
+    Arguments:
+        auth_user_id (int) - Used to uniquely identify the user
+
+    Exceptions:
+        InputError - Occurs when:
+            Given user_id does not exist in the system
+
+    Return Value:
+        Returns {} when user is not a part of any channel
+        Returns a dictionary to a list of channels which are smaller dictionaries
+        that contains channel_id and name otherwise
+        Example:
+        return {
+            'channels': [
+                {
+                    'channel_id': 2
+                    'name': "Channel 2"
+                },
+                {
+                    'channel_id': 6
+                    'name': "Channel Private"
+                }
+            ]
+        }
+    '''
     user_data = data_store.get_data()['users']
     user_exists = False
     for user in user_data:
@@ -28,6 +57,34 @@ def channels_list_v1(auth_user_id):
     return channel_dict
 
 def channels_listall_v1(auth_user_id):
+    '''
+    Function will take in an user_id and list all the public and private channels
+    the user is in
+    Arguments:
+        auth_user_id (int) - Used to uniquely identify the user
+
+    Exceptions:
+        InputError - Occurs when:
+            Given user_id does not exist in the system
+
+    Return Value:
+        Returns {} when user is not a part of any channel
+        Returns a dictionary to a list of channels which are smaller dictionaries
+        that contains channel_id and name otherwise
+        Example:
+        return {
+            'channels': [
+                {
+                    'channel_id': 2
+                    'name': "Channel 2"
+                },
+                {
+                    'channel_id': 6
+                    'name': "Channel Private"
+                }
+            ]
+        }
+    '''
     user_data = data_store.get_data()['users']
     user_exists = False
     for user in user_data:
@@ -54,19 +111,39 @@ def channels_listall_v1(auth_user_id):
     return channel_dict
 
 def channels_create_v1(auth_user_id, name, is_public):
+    '''
+    Function will take in an user_id and list all the public and private channels \
+    the user is in
+
+    Arguments:
+        auth_user_id (int) - Used to uniquely identify the user
+
+    Exceptions:
+        InputError - Occurs when:
+            Given user_id does not exist in the system
+            Channel name is taken
+            Channel name is smaller than 1 character or greater than 20
+
+    Return Value:
+        Returns a dicionary containing the new channel_id
+        Example:
+        return {
+            'channel_id': 4
+        }
+    '''
     user_data = data_store.get_data()['users']
     user_exists = False
     for user in user_data:
         if auth_user_id == user['id']:
             user_exists = True
             curr_user = user
-    
+
     if not user_exists:
         raise InputError("User doesn't exist")
-    
+
     if len(name) < 1 or len(name) > 20:
         raise InputError("Invalid channel name")
-    
+
     channel_data = data_store.get_data()['channels']
     for channel in channel_data:
         if name == channel['name']:
