abe2917f099f3f44bcff0f72f8db4c62dddacf08
Joseph <joseph.shao2003@gmail.com>
2021-09-30 11:10:43 +1000

    Deleted listall by accident, now replaced

--- src/channels.py
+++ src/channels.py
@@ -8,6 +8,16 @@ def channels_list_v1(auth_user_id):
         ],
     }
 
+def channels_listall_v1(auth_user_id):
+    return {
+        'channels': [
+        	{
+        		'channel_id': 1,
+        		'name': 'My Channel',
+        	}
+        ],
+    }
+
 def channels_create_v1(auth_user_id, name, is_public):
     '''
     Function will take in an user_id and list all the public and private channels \
