a5be2af524b82f16775a21837c5821f87b35f09c
Weixuan <z5364271@ad.unsw.edu.au>
2021-10-10 17:06:51 +1100

    Added all new functions specified in it2. All v1 functions still remail v1

---
+++ src/admin.py
@@ -0,0 +1,12 @@
+'''Contains all admin related functions, such as removing users and permissions'''
+from src.data_store import data_store
+from src.error import InputError
+from src.error import AccessError
+
+def admin_user_remove_id(token, u_id):
+    pass
+    #Return type {}
+    
+def admin_userpermission_change_v1(token, u_id, permission_id):
+    pass
+    #Return type {}
