af4d263c807c17b54944e290e98225d6f701814d
John Henderson <z5368143@ad.unsw.edu.au>
2021-10-18 16:42:45 +1100

    Added comments and cleaed up

--- src/data_store.py
+++ src/data_store.py
@@ -37,7 +37,7 @@ Example Storage: (Joseph)
                 'passwords': '123123',
                 'handle' : 'AdminUser1234567891011',
                 'channels' : [1, 2]
-                'token': "OLIUEFKSEJF.IEUFHKESF.Iuhflskejhf"
+                'token': ["OLIUEFKSEJF.IEUFHKESF.Iuhflskejhf"]
             },
         ],
         'channels': [
