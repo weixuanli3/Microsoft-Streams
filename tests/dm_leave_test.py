a8f930b0f6bb68c8f4e377775871694cabcdb85a
Joseph <joseph.shao2003@gmail.com>
2021-10-18 13:27:51 +1100

    Fixed fixture bug and changed tsome v2 function calls back into v1 function calls

--- tests/dm_leave_test.py
+++ tests/dm_leave_test.py
@@ -1,15 +1,15 @@
 #import pytest
-#from src.auth import auth_register_v2
+#from src.auth import auth_register_v1
 #from src.dm import dm_create_v1, dm_list_v1, dm_remove_v1, dm_leave_v1
 #from src.error import AccessError, InputError
 #
 #@pytest.fixture
 #def def_setup():
 #    clear_v1()
-#    owner = auth_register_v2("john.doe@unsw.com", "bruhdems", "John", "Doe")
-#    user1 = auth_register_v2("patrick.liang@unsw.com", "katrick", "Patrick", "Liang")
-#    user2 = auth_register_v2("john.citizen@unsw.com", "password", "John", "Citizen")
-#    user3 = auth_register_v2("nangston.w@unsw.com", "nangwei", "Wynston", "Wang")
+#    owner = auth_register_v1("john.doe@unsw.com", "bruhdems", "John", "Doe")
+#    user1 = auth_register_v1("patrick.liang@unsw.com", "katrick", "Patrick", "Liang")
+#    user2 = auth_register_v1("john.citizen@unsw.com", "password", "John", "Citizen")
+#    user3 = auth_register_v1("nangston.w@unsw.com", "nangwei", "Wynston", "Wang")
 #    own_tok = owner['token']
 #    own_id = owner['auth_user_id']
 #    u1_tok = user1['token']
@@ -21,7 +21,7 @@
 #    dm_id3 = dm_create_v1(u2_id, [])['dm_id']
 #    return (own_tok, own_id, u1_tok, u1_id, u2_tok, u2_id, dm_id1, dm_id2, dm_id3)
 #
-#def test_normal_leave():
+#def test_normal_leave(def_setup):
 #    own_tok, own_id, u1_tok, u1_id, u2_tok, u2_id, dm_id1, dm_id2, dm_id3 = def_setup()
 #    owner = {
 #        'u_id': own_id,
@@ -59,16 +59,16 @@
 #    with pytest.raises(AccessError):
 #        dm_leave_v1(u1_tok, dm_id3)
 #
-#def test_invalid_dm_id():
+#def test_invalid_dm_id(def_setup):
 #    own_tok, own_id, u1_tok, u1_id, u2_tok, u2_id, dm_id1, dm_id2, dm_id3 = def_setup()
 #    with pytest.raises(InputError):
 #        dm_leave_v1(u1_tok, dm_id1 + dm_id2 + 1)
 #    with pytest.raises(InputError):
 #        dm_leave_v1(u1_tok, -1)
 #
-#def test_valid_dm_id_not_a_member():
+#def test_valid_dm_id_not_a_member(def_setup):
 #    own_tok, own_id, u1_tok, u1_id, u2_tok, u2_id, dm_id1, dm_id2, dm_id3 = def_setup()
-#    user3 = auth_register_v2("nangston.w@unsw.com", "nangwei", "Wynston", "Wang")
+#    user3 = auth_register_v1("nangston.w@unsw.com", "nangwei", "Wynston", "Wang")
 #    u3_tok = user3['token']
 #    u3_id = user3['auth_user_id']
 #    with pytest.raises(AccessError):
