f2f7b88e92bb511e292b7ef8ebab86717b8ca695
z5361821 <z5361821@ad.unsw.edu.au>
2021-10-20 02:01:37 +1100

    copied from another branch

--- src/dm.py
+++ src/dm.py
@@ -3,21 +3,43 @@ from src.data_store import data_store, get_u_id, update_permanent_storage
 from src.error import InputError
 from src.error import AccessError
 
-def dm_create_v1(token, *u_ids):
-    
+def dm_create_v1(token, u_ids):
+
     user_data = data_store.get_data()['users']
-    
-    all_valid = False
-    
-    # check all u_ids are valid
-    for u_id in u_ids:
-        for user in user_data:
+        
+    user_exists = []
+    for user in user_data:
+        for u_id in u_ids:
             if u_id == user['id']:
-                all_valid = True
-            else:
-                all_valid = False
-        if not all_valid:
-            raise InputError("At least one of the u_ids is invalid")
+                user_exists.append(1)
+
+    if user_exists.count(1) != len(u_ids):
+        raise InputError("User doesn't exist")
+    
+    # for id in user_data_ids:
+    #     if id not in user_data_ids:
+    #         raise InputError("At least one of the u_ids is invalid")
+    
+    # all_valid = False
+    
+    # # check all u_ids are valid
+    # for u_id in u_ids:
+    #     for user in user_data:
+    #         if u_id == user['id']:
+    #             all_valid = True
+    #         else:
+    #             all_valid = False
+    # if not all_valid:
+    #     raise InputError("At least one of the u_ids is invalid")
+    
+    # check if token is valid
+    valid_token = False
+    for user in user_data:
+        if token in user['token']:
+            valid_token = True
+
+    if not valid_token:
+        raise InputError("Invalid Token")
     
     dm_data = data_store.get_data()['DMs']
     dm_id = len(dm_data) + 1
@@ -59,7 +81,7 @@ def dm_create_v1(token, *u_ids):
     
     update_permanent_storage()
     
-    return dm_id
+    return {'dm_id': dm_id}
     #Return type {dm_id}
 
 def dm_list_v1(token):
@@ -74,7 +96,7 @@ def dm_list_v1(token):
     
     update_permanent_storage()
     
-    return dms
+    return {'dms': dms}
     #Return type {dms}
     
 def dm_remove_v1(token, dm_id):
@@ -146,7 +168,7 @@ def dm_leave_v1(token, dm_id):
                 dm['members'].remove(user_id)
                 user_in_dm = True
 
-    if not user_in_dm:
+    if dm_id_valid and not user_in_dm:
         raise AccessError("User is not a member of the DM")
 
     if not dm_id_valid:
@@ -175,7 +197,6 @@ def dm_messages_v1(token, dm_id, start):
         if dm_id == dm['dm_id']:
             dm_id_valid = True
             if user_id in dm['members']:
-                
                 user_in_dm = True
 
     if not user_in_dm:
@@ -184,6 +205,36 @@ def dm_messages_v1(token, dm_id, start):
     if not dm_id_valid:
         raise InputError("dm_id does not refer to a valid DM")
     
+    msg = []
+    
+    for dm in dm_data:
+        if dm_id == dm['dm_id']:
+            msg.append(dm['messages'])
+    
+    msg = msg.reverse()
+    
+    if start < 0:
+        raise InputError("Start cannot be negative")
+    elif not msg:
+        raise InputError("Start is greater than the total number of messages in the channel")
+    elif start > len(msg) - 1:
+        raise InputError("Start is greater than the total number of messages in the channel")
+
+    # If there are e.g. 50 messages and start = 30, can only return 20, end = -1
+    if len(msg) < (start + 50):
+        return_messages = msg[start:-1]
+        end = -1
+    else: # If there are e.g. 100 messages and start = 30, returns 30 up to 80, end = 80
+        return_messages = msg[start:start + 50]
+        end = start + 50
+
+    return_dictionary['messages'].append(return_messages)
+    return_dictionary['start'] = start
+    return_dictionary['end'] = end
+
+    update_permanent_storage()
+
     return return_dictionary
+        
     #Return type {messages, start, end}
     
