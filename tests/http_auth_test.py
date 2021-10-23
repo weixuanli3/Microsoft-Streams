d9da328ad9a1ae2c9f11643a5a5377ee6ba5964d
John Henderson <z5368143@ad.unsw.edu.au>
2021-10-23 22:44:37 +1100

    Added http tests for auth

--- tests/http_auth_test.py
+++ tests/http_auth_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.error import InputError
 from src.other import clear_v1
-from src.request_helper_functions import admin_user_remove_req, auth_register_req, clear_req
+from src.request_helper_functions import admin_user_remove_req, auth_login_req, auth_logout_req, auth_register_req, clear_req
 from src.data_store import data_store
 
 #------------------------------------------------------------
@@ -82,3 +82,85 @@ def test_longer_handle():
     auth_register_req("john.doe@unsw.edu.au","password","John","Doe")
     auth_register_req("john.do2@unsw.edu.au","password","John","Doe")
     auth_register_req("john.do3@unsw.edu.au","password","John","Doe")
+
+#------------------------------------------------------------
+# Log in
+# This block of code deals with the auth_login_v1 function
+# from auth.py
+
+def test_auth_login_email_not_registered():
+    clear_v1()
+    register_test = auth_login_req("john.doe10@unsw.edu.au","password")
+    assert register_test['code'] == InputError.code
+
+def test_auth_login_email_empty():
+    clear_v1()
+    auth_register_req("john.doe11@unsw.edu.au","password","John","Doe")
+    register_test = auth_login_req("","password")
+    assert register_test['code'] == InputError.code
+
+# The email is a user, but has the wrong password
+def test_auth_login_incorrect_password():
+    clear_v1()
+    auth_register_req("john.doe12@unsw.edu.au","password","John","Doe")
+    register_test = auth_login_req("john.doe12@unsw.edu.au","password123")
+    assert register_test['code'] == InputError.code
+
+def test_auth_login_correct_password():
+    clear_v1()
+    user_id = auth_register_req("john.doe13@unsw.edu.au","password","John","Doe")["auth_user_id"]
+    assert auth_login_req("john.doe13@unsw.edu.au","password")['auth_user_id'] == user_id
+
+def test_auth_login_bad_login_good_login():
+    clear_v1()
+    user_id = auth_register_req("john.doe@unsw.edu.au","password","John","Doe")["auth_user_id"]
+
+    # Should not work
+    register_test = auth_login_req("john.doe@unsw.edu.au","password1")
+    assert register_test['code'] == InputError.code
+
+    # Should return corrent login user ID
+    assert auth_login_req("john.doe@unsw.edu.au","password")["auth_user_id"] == user_id
+
+def test_auth_login_password_empty():
+    clear_v1()
+    auth_register_req("john.doe12@unsw.edu.au","password","John","Doe")
+    register_test = auth_login_req("john.doe12@unsw.edu.au","")
+    assert register_test['code'] == InputError.code
+
+def test_auth_login_pass_different_user():
+    clear_v1()
+    auth_register_req("john.doe1@unsw.edu.au","password1","John","Doe")
+    auth_register_req("john.doe2@unsw.edu.au","password2","John","Doe")
+    register_test = auth_login_req("john.doe1@unsw.edu.au", "password2")
+    assert register_test['code'] == InputError.code    
+
+#------------------------------------------------------------
+# Log out
+# This block of code deals with the auth_logout_v1 function
+# from auth.py
+
+# POSSIBLE ASSUMPTION RASIE INPUT ERROR?
+def test_invalid_token():
+    clear_v1()
+    auth_register_req("john.doe12@unsw.edu.au","password","John","Doe")
+    auth_login_req("john.doe12@unsw.edu.au","password")['token']
+    
+    register_test = auth_logout_req(923564)
+    assert register_test['code'] == InputError.code    
+
+# CANNOT CALL FUNCTION WITHOUT PROPER INPUT, ERROR NOT ON SERVER SIDE   
+# def test_empty_token():
+#     clear_v1()
+#     auth_register_v1("john.doe12@unsw.edu.au","password","John","Doe")
+#     token = auth_login_v1("john.doe12@unsw.edu.au","password")['token']
+#     with pytest.raises(InputError): 
+#       auth_logout_v1()
+
+def test_valid_token():
+    clear_v1()
+    auth_register_req("john.doe12@unsw.edu.au","password","John","Doe")
+    token = auth_login_req("john.doe12@unsw.edu.au","password")['token']
+    assert auth_logout_req(token) == {}
+    clear_v1()
+
