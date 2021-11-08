cf7d5173e5731a348437f676a96f7f93f0cd3c59
Joseph <joseph.shao2003@gmail.com>
2021-11-08 23:04:51 +1100

    Added code for workspace_stats

--- src/other.py
+++ src/other.py
@@ -36,7 +36,13 @@ def clear_v1():
         'channels': [],
         'global_owners' : [],
         'DMs': [],
-        'msgs' : []
+        'msgs' : [],
+        'workspace_stats': {
+            'channels_exist': [{'num_channels_exist': 0, 'time_stamp': datetime.now()}],
+            'dms_exist': [{'num_dms_exist': 0, 'time_stamp': datetime.now()}],
+            'messages_exist': [{'num_messages_exist': 0, 'time_stamp': datetime.now()}],
+            'utilization_rate': 0
+        }
     }
     data_store.set(store)
 
