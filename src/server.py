04487914fb4e6e7fefc68955fb7ab3715e785797
Joseph <joseph.shao2003@gmail.com>
2021-11-13 01:17:42 +1100

    Fixed get request fot standup active

--- src/server.py
+++ src/server.py
@@ -370,7 +370,7 @@ def standup_start():
 @APP.route("/standup/active/v1", methods=['GET'])
 def standup_active():
     token = request.args.get('token')
-    channel_id = request.args.get('channel_id')
+    channel_id = int(request.args.get('channel_id'))
     return json.dumps(standup_active_v1(token, channel_id))
 
 # Send a message to be buffered
