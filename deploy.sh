a3a435707862f910458e821f7e3fb7d465779d17
Joseph <joseph.shao2003@gmail.com>
2021-11-15 03:08:01 +1100

    Deployment functional

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 WORKING_DIRECTORY="~/www/cs1531deploy"
 
-USERNAME="TODO-TODO-TODO"
-SSH_HOST="TODO-TODO-TODO"
+USERNAME="beaglew15a"
+SSH_HOST="ssh-beaglew15a.alwaysdata.net"
 
 rm -rf ./**/__pycache__ ./**/.pytest_cache > /dev/null
-scp -r ./requirements.txt ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
+scp -r ./requirements.txt ./src ./other_functions "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
 ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && source env/bin/activate && pip3 install -r requirements.txt"
